274f08effb951a126d27e2216b198300eef884dc
ccastillo_tegik <ccastillo@tegik.com>
2018-11-24 13:53:54 -0600

    el objecto card tiene los objectos header, body, fooster

---
+++ src/app/model/card.ts
@@ -0,0 +1,11 @@
+
+import { Fooster} from './fooster';
+import { Header} from './header';
+import { Body} from './body';
+
+
+export interface Card {
+	header: Header,
+	body: Body,
+	fooster: Fooster
+}
